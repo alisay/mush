ff3bf0759d0463fbe2aaacf5d16c1c23060c2217
Alisa <alisa.blakeney@gmail.com>
2020-08-03 12:23:27 +1000

    revise conditional routes for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(profile)
+
+        if(current_user.profile)
+            new_mushroom_path
+        else
+            new_profile_path || root_path
+        end
+    end 
+    
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :profiles
   devise_for :users
   root 'home#page'
+  get '/search' => 'pages#search', :as => 'search_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
