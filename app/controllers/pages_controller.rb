6bae79230019f26f1cb96ece33b33edf13718b93
Alisa <alisa.blakeney@gmail.com>
2020-08-04 15:38:59 +1000

    refine search function to add link to view results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
 
     def search  
+        if params[:search].blank?  
+            redirect_to(root_path, alert: "Please enter a search term") and return  
+        else  
+            @parameter = params[:search].downcase  
+            @results = Mushroom.all.where("lower(flavour) LIKE :search", search: "%#{@parameter}%")
+        end  
     end
 
 end
